4b3c2b3e654a4ee6c5d62dbbe224ef9e20965539
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-10-12 17:24:06 +0800

    80——增加一种题解

--- src/array/Solution80.java
+++ src/array/Solution80.java
@@ -37,10 +37,31 @@ public class Solution80 {
 
     }
 
+    //利用增强for循环
+    public int removeDuplicates1(int[] nums) {
+        if (nums!=null&&nums.length!=0){
+            int count=0;
+            for (int num:nums){
+                if (count<2||num>nums[count-2])
+                    nums[count++]=num;
+
+            }
+            return count;
+
+
+
+        }
+        else
+            return 0;
+
+    }
+
+
+
     @Test
     public void test() {
         int[] ints = {0,0,1,1,1,1,2,3,3};
-        int i = removeDuplicates(ints);
+        int i = removeDuplicates1(ints);
         System.out.println(i);
         for (int t : ints) {
             System.out.print(t + " ");
