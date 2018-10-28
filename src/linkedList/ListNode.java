20028ebdb3684780ae26b313d71aee7f5b7281ec
IvanLu1024 <501275190@qq.com>
2018-10-28 22:28:34 +0800

    链表类

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+/**
+ * 链表的结点
+ *
+ * @author
+ * @create 2018-10-28 20:18
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
