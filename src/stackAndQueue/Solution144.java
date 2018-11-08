73ad0e4fc823882ddb84581ead9b2d8f1a7f07c4
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-11-08 17:54:21 +0800

    144

---
+++ src/stackAndQueue/Solution144.java
@@ -0,0 +1,70 @@
+package stackAndQueue;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution144 {
+
+    class Command{
+        int command;//0:表示继续遍历；1:表示加入集合
+        TreeNode node;
+
+        public Command(int command, TreeNode node) {
+            this.command = command;
+            this.node = node;
+        }
+    }
+
+    private List<Integer> res=new ArrayList<>();
+    //递归式
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root!=null){
+            res.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return res;
+    }
+
+    //迭代式
+    public List<Integer> preorderTraversal1(TreeNode root){
+        if (root==null){
+            return res;
+        }
+        Stack<Command> stack=new Stack<>();
+        stack.push(new Command(0,root));
+        while (!stack.empty()){
+            Command command = stack.pop();
+            if (command.command==1){
+                res.add(command.node.val);
+            }else {
+                if (command.node.right!=null){
+                    stack.push(new Command(0,command.node.right));
+                }
+                if (command.node.left!=null){
+                    stack.push(new Command(0,command.node.left));
+                }
+                stack.push(new Command(1,command.node));
+            }
+        }
+        return res;
+
+    }
+    @Test
+    public void test(){
+        TreeNode root = new TreeNode(0);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3=new TreeNode(3);
+        root.left=n1;
+        root.right=n2;
+        n1.left=n3;
+        List<Integer> res = preorderTraversal1(root);
+        System.out.println(res);
+
+
+    }
+}
---
+++ src/stackAndQueue/TreeNode.java
@@ -0,0 +1,8 @@
+package stackAndQueue;
+
+public class TreeNode {
+      int val;
+     TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
