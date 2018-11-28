f348001f6de401716395c11e61342ea4931fb86e
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-11-28 16:21:50 +0800

    70

---
+++ src/dp/Solution70.java
@@ -0,0 +1,18 @@
+package dp;
+
+import java.util.Arrays;
+
+public class Solution70 {
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo,-1);
+        memo[0]=1;
+        memo[1]=1;
+        for (int i = 2; i <= n; i++) {
+            memo[i]=memo[i-1]+memo[i-2];
+        }
+        return memo[n];
+    }
+
+
+}
