5ad8d2a2b1bd6fc2cdf48e0e092bd30ab816c018
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-12-13 11:31:17 +0800

    148--链表的归并排序

---
+++ src/linkedList/Solution148.java
@@ -0,0 +1,68 @@
+package linkedList;
+
+import org.junit.Test;
+
+/**
+ * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ *
+ * 示例 1:
+ *
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ *
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ *
+ */
+public class Solution148 {
+    //自顶向上的归并排序
+    public ListNode sortList(ListNode head) {
+        if (head==null||head.next==null){
+            return head;
+        }
+        ListNode fast=head.next,slow=head;
+        while (fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode head2=slow.next;
+        slow.next=null;
+        head = sortList(head);
+        head2=sortList(head2);
+        return merge(head,head2);
+    }
+    private ListNode merge(ListNode a,ListNode b){
+        ListNode dummyHead=new ListNode(-1);
+        ListNode p1=a,p2=b,p=dummyHead;
+        while (p1!=null&&p2!=null){
+            if (p1.val<p2.val){
+                p.next=p1;
+                p=p.next;
+                p1=p1.next;
+                p.next=null;
+            }else {
+                p.next=p2;
+                p=p.next;
+                p2=p2.next;
+                p.next=null;
+            }
+        }
+        if (p1!=null){
+            p.next=p1;
+        }
+        if (p2!=null){
+            p.next=p2;
+        }
+        return dummyHead.next;
+
+    }
+    @Test
+    public void test(){
+        int[] arr={4,2,1,3};
+        ListNode head = LinkedListUtils.createLinkedList(arr, arr.length);
+        LinkedListUtils.printLinkedList(head);
+        ListNode res = sortList(head);
+        LinkedListUtils.printLinkedList(res);
+    }
+}
