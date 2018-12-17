4de18b79278eb279c53e9aa508781463d6dbf710
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-12-17 14:20:36 +0800

    调整

--- src/linkedList/ListNode.java
+++ src/linkedList/ListNode.java
@@ -7,8 +7,8 @@ package linkedList;
  * @create 2018-10-28 20:18
  **/
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int val) {
         this.val = val;
