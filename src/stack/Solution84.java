f7e0195e0ac09b6a13c74ffa9f5b04f7a07c03dd
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2019-01-09 15:13:32 +0800

    84

--- src/stack/Solution84.java
+++ src/stack/Solution84.java
@@ -21,19 +21,26 @@ public class Solution84 {
         if (n==1){
             return heights[0];
         }
-        //栈来记录递增序列的下标
+        //栈来记录从栈顶到栈底递增序列的下标
         Stack<Integer> stack = new Stack<>();
         int res=0;
+        //处理的柱子宽度为0-n
         for (int i = 0; i <= n; i++) {
-            //
+            //为了避免数组越界，将高度进行了统一处理
             int num=i==n?0:heights[i];
+            //如果当前遍历的栈为空或者高度大于或等于栈顶元素的值的时候，入栈
             if (stack.empty()||num>=heights[stack.peek()]){
                 stack.push(i);
-            }else {
+            }//当前遍历的高度小于栈顶的值的时候，出栈
+            else {
                 int top = stack.pop();
                 //计算当前的宽度
+                //当栈为空的时候表明，当前高度为最小高度
                 int w = stack.empty()?i:i-1-stack.peek();
+                //记录最大面积
                 res=Math.max(res,w*heights[top]);
+
+                //为了从当前位置继续开始
                 i--;
             }
         }
