513d04e8581b52ce7595176e97698eb4a195ed2b
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2019-01-22 20:59:21 +0800

    5

--- src/dp/Solution5.java
+++ src/dp/Solution5.java
@@ -54,10 +54,37 @@ public class Solution5 {
         }
         return s.substring(start,start+maxLen);
     }
+
+    public String longestPalindrome1(String s){
+        if (s==null||s.length()==0){
+            return "";
+        }
+        int start=0,end=0;
+        for (int i = 0; i < s.length(); i++) {
+            //注意i的位置
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1,len2);
+            if (len>end-start){
+                //确保start和i的距离
+                start = i - (len-1)/2;
+                end = i +(len/2);
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+    private int expand(String s,int l ,int r){
+        while (l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+        }
+        return r-l-1;
+    }
     @Test
     public void test(){
-        String s="babad";
-        String s1 = longestPalindrome(s);
+        String s="cbbd";
+        String s1 = longestPalindrome1(s);
         System.out.println(s1);
     }
 }
