6ec840d81c9681281e999cb8ed8ed410c76e07b8
IvanLu1024 <501275190@qq.com>
2019-03-06 21:52:10 +0800

    114

--- src/tree/Solution114.java
+++ src/tree/Solution114.java
@@ -2,7 +2,6 @@ package tree;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -37,26 +36,30 @@ public class Solution114 {
 
 
     public void flatten(TreeNode root) {
-        TreeNode head = new TreeNode(-1);
-        head.right=root;
-        TreeNode node = head;
-        while (node.right!=null){
-            node=node.right;
-            if (node.left!=null){
-                TreeNode end = node.left;
-                while (end.right!=null){
-                    end=end.right;
-                }
-                TreeNode t = node.right;
-                node.right=node.left;
-                node.left=null;
-                end.right=t;
-            }
+        if(root==null){
+            return ;
         }
+        TreeNode node=root;
+        //不断地将右子树与放入左子树中与根节点相邻的结点的右边
+        while (node!=null){
+            if (node.left!=null) {
+                TreeNode left = node.left;
+                //寻找左子树中最右边的结点就是和根节点相邻的结点
+                while (left.right != null) {
+                    left = left.right;
+                }
 
+                //将右子树放入该节点的右侧
+                left.right = node.right;
+                //将左子树整体放入根节点的右边
+                node.right = node.left;
+                node.left = null;
+            }
+            node = node.right;
+        }
     }
 
-    private List<TreeNode> dfs(TreeNode root){
+    private List<TreeNode> bfs(TreeNode root){
         List<TreeNode> res=new LinkedList<>();
         if (root==null){
             return res;
@@ -79,7 +82,7 @@ public class Solution114 {
     }
 
     public void flatten1(TreeNode root) {
-        List<TreeNode> res = dfs(root);
+        List<TreeNode> res = bfs(root);
         for (int i = 0; i < res.size()-1; i++) {
             res.get(i).right=res.get(i+1);
         }
