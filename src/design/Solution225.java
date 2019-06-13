96600b74608379c7f485a2ac16d50040daeb00f7
IvanLu1024 <501275190@qq.com>
2019-06-13 20:03:18 +0800

    225更新

--- src/design/Solution225.java
+++ src/design/Solution225.java
@@ -1,39 +1,40 @@
 package design;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution225 {
 
     class MyStack {
 
-        private LinkedList<Integer> data;
-
+        Queue<Integer> queue;
         /** Initialize your data structure here. */
         public MyStack() {
-            data = new LinkedList<>();
-
+            queue =new LinkedList<>();
         }
 
         /** Push element x onto stack. */
+        //将入队顺序逆序
         public void push(int x) {
-            data.add(x);
+            queue.offer(x);
+            for (int i = 0; i < queue.size() - 1; i++) {
+                queue.offer(queue.poll());
+            }
         }
 
         /** Removes the element on top of the stack and returns that element. */
         public int pop() {
-            int res = data.getLast();
-            data.removeLast();
-            return res;
+            return queue.poll();
         }
 
         /** Get the top element. */
         public int top() {
-            return data.getLast();
+            return queue.peek();
         }
 
         /** Returns whether the stack is empty. */
         public boolean empty() {
-            return data.isEmpty();
+            return queue.isEmpty();
         }
     }
 }
