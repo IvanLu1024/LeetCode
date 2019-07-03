cbbf11309f1e769f5f746e33c28a6daa0cb2858e
IvanLu1024 <501275190@qq.com>
2019-07-03 17:06:54 +0800

    268

--- src/array/Solution268.java
+++ src/array/Solution268.java
@@ -28,10 +28,27 @@ public class Solution268 {
         }
         return 0;
     }
+
+    public int missingNumber1(int[] nums) {
+        if(nums==null||nums.length==0){
+            return 0;
+        }
+
+        int n=nums.length;
+        //预期结果为等差数列：{1+2+...+n}的结果
+        int res=n*(n+1)/2;
+        //缺失的数字就是和预期结果之间的差
+        for(int i=0;i<n;i++){
+            res-=nums[i];
+        }
+        return res;
+    }
     @Test
     public void test(){
-        int[] nums={9,6,4,2,3,5,7,0,1};
-        int i = missingNumber(nums);
+        /*{3,0,1}
+        * {9,6,4,2,3,5,7,0,1}*/
+        int[] nums={3,0,1,4};
+        int i = missingNumber1(nums);
         System.out.println(i);
     }
 }
