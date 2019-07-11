e608dcbdead913ca4296cfe7c1ce46e21b0f1e12
IvanLu1024 <501275190@qq.com>
2019-07-11 16:22:01 +0800

    213

--- src/dp/Solution213.java
+++ src/dp/Solution213.java
@@ -10,27 +10,25 @@ public class Solution213 {
     //与198题类似，相当于做两次抢劫，一次的范围是[0,n-2],
     // 另一次的范围是[1,n-1]，取两次抢劫的最大值即可
     public int rob(int[] nums) {
-        int n=nums.length;
-        if (n==0){
+        if(nums==null||nums.length==0)
             return 0;
-        }
-        if(n==1){
+        int n=nums.length;
+        if(n==1)
             return nums[0];
-        }
-        if(n==2){
+        if(n==2)
             return Math.max(nums[0],nums[1]);
-        }
-        return Math.max(rob(nums,0,n-2),rob(nums,1,n-1));
-
+        int res=Math.max(rob(nums,0,n-2),rob(nums,1,n-1));
+        return res;
     }
-    private int rob(int[] nums,int start,int end){
-        int n=end-start+1;
-        int[] memo=new int[n];
-        memo[0]=nums[start];
-        for (int i = 1; i <n ; i++) {
-            memo[i]=Math.max(memo[i-1],nums[start+i]+(i-2>=0?memo[i-2]:0));
+    //抢劫：起始点为l,终点为h的最大收益
+    private int rob(int[] nums,int l,int h){
+        int[] memo=new int[h-l+1];
+        memo[0]=nums[l];
+        memo[1]=Math.max(nums[l],nums[l+1]);
+        for(int i=2;i<=h-l;i++){
+            memo[i]=Math.max(nums[i+l]+memo[i-2],memo[i-1]);
         }
-        return memo[n-1];
+        return memo[h-l];
     }
     @Test
     public void test(){
